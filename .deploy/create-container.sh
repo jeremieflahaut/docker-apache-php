0d3d7c6ebf8bf8ba5335f98d8982754ff00012e6
jeremie <jeremieflahaut@gmail.com>
2023-01-28 15:20:40 +0100

    php-fpm

---
+++ .deploy/create-container.sh
@@ -0,0 +1,91 @@
+#!/usr/bin/env bash
+
+export DEBIAN_FRONTEND=noninteractive
+export COMPOSER_ALLOW_SUPERUSER=1
+
+# Mettre a jour les paquets
+echo "update distribution..."
+apt-get update && apt-get install -y apt-utils && apt-get upgrade -y
+
+# install git et autres logiciels
+echo "install packages..."
+apt-get install -y wget git ntp ntpdate sudo nano curl cron rsyslog logrotate
+
+# Synchro horloge
+echo "synchro horloge..."
+/etc/init.d/ntp restart
+cp /etc/localtime /etc/localtime.bak
+cp /usr/share/zoneinfo/Europe/Paris /etc/localtime
+date
+
+#Install apache
+echo "Install apache..."
+apt-get install apache2 -y
+cp .deploy/php/php.ini /usr/local/etc/php/conf.d/app.ini
+cp .deploy/apache2/vhost.conf /etc/apache2/sites-available/000-default.conf
+cp .deploy/apache2/apache.conf /etc/apache2/conf-available/z-app.conf
+a2enconf z-app
+a2enmod proxy_fcgi
+
+#Install php-fpm
+echo "Install php..."
+apt-get install -y build-essential lsb-release ca-certificates apt-transport-https software-properties-common memcached libmemcached-tools zlib1g-dev
+echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/sury-php.list
+wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
+apt-get update && apt-get install -y openssl php8.2 php8.2-common php8.2-fpm php-pear php8.2-dev php8.2-memcached php8.2-curl php8.2-bcmath php8.2-ctype php8.2-fileinfo php8.2-mbstring php8.2-mysql php8.2-sqlite3 php8.2-xml php8.2-zip php8.2-tokenizer php8.2-gd php8.2-imagick \
+&& pecl channel-update pecl.php.net \
+&& pecl install memcached redis mongodb xdebug \
+&& touch /etc/php/8.2/mods-available/memcached.ini && echo extension=memcached.so > /etc/php/8.2/mods-available/memcached.ini \
+&& ln -s /etc/php/8.2/mods-available/memcached.ini /etc/php/8.2/fpm/conf.d/memcached.ini \
+&& ln -s /etc/php/8.2/mods-available/memcached.ini /etc/php/8.2/cli/conf.d/memcached.ini \
+&& touch /etc/php/8.2/mods-available/redis.ini && echo extension=redis.so > /etc/php/8.2/mods-available/redis.ini \
+&& ln -s /etc/php/8.2/mods-available/redis.ini /etc/php/8.2/fpm/conf.d/redis.ini \
+&& ln -s /etc/php/8.2/mods-available/redis.ini /etc/php/8.2/cli/conf.d/redis.ini \
+&& touch /etc/php/8.2/mods-available/mongodb.ini && echo extension=mongodb.so > /etc/php/8.2/mods-available/mongodb.ini \
+&& ln -s /etc/php/8.2/mods-available/mongodb.ini /etc/php/8.2/fpm/conf.d/mongodb.ini \
+&& ln -s /etc/php/8.2/mods-available/mongodb.ini /etc/php/8.2/cli/conf.d/mongodb.ini \
+&& touch /etc/php/8.2/mods-available/xdebug.ini && echo zend_extension=xdebug.so > /etc/php/8.2/mods-available/xdebug.ini \
+&& ln -s /etc/php/8.2/mods-available/xdebug.ini /etc/php/8.2/fpm/conf.d/xdebug.ini \
+&& ln -s /etc/php/8.2/mods-available/xdebug.ini /etc/php/8.2/cli/conf.d/xdebug.ini
+
+cp .deploy/index.php /var/www/html/public/index.php
+
+#Install composer
+echo "Install composer..."
+curl -sS https://getcomposer.org/installer -o composer-setup.php
+HASH=`curl -sS https://composer.github.io/installer.sig`
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+composer -V
+rm composer-setup.php
+
+#Clean
+rm -r /var/lib/apt/lists/* && apt-get autoremove -y && apt-get clean all
+
+#Default user
+groupadd --gid 1001 debian && useradd --uid 1001 --gid 1001 --create-home --shell /bin/bash debian
+echo debian:debian | chpasswd
+echo "%debian ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+chown -R debian:debian /root
+chown -R debian:debian /var/www/html
+echo "alias ls='ls -alh'" >> /home/debian/.bashrc
+echo "alias lh='ls'" >> /home/debian/.bashrc
+
+#Cron & Logrotate
+cp "/.deploy/logrotate/logrotate_apache.conf" /etc/logrotate.d/apache2
+cp "/.deploy/logrotate/logrotate_laravel.conf" /etc/logrotate.d/laravel
+chown -R root:debian /var/log/apache2
+chmod -R 775 /var/log/apache2
+
+mkdir -p /var/log/laravel
+chown -R root:debian /var/log/laravel
+chmod -R 775 /var/log/laravel
+
+crontab -u debian -l > /tmp/mycron
+echo "0 0 * * * sudo /usr/sbin/logrotate -f /etc/logrotate.conf" >> /tmp/mycron
+echo "* * * * * /usr/local/bin/php /var/www/html/artisan schedule:run >> /var/log/cron.log 2>&1" >> /tmp/mycron
+crontab -u debian /tmp/mycron
+rm /tmp/mycron
+chown debian:crontab /var/spool/cron/crontabs/debian
+echo "Cron install done."
+
--- Dockerfile
+++ Dockerfile
@@ -1,25 +1,15 @@
-FROM php:8.2-apache
+FROM debian:buster-slim
 
-ENV COMPOSER_ALLOW_SUPERUSER=1
-RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
-RUN composer --version
+COPY .deploy /.deploy
 
-RUN apt-get update -qq && apt-get install -qy git gnupg unzip zip sudo
-RUN docker-php-ext-install -j$(nproc) opcache pdo_mysql
+COPY startup.sh /var/bin/startup.sh
 
-COPY .deploy/php/php.ini /usr/local/etc/php/conf.d/app.ini
-COPY .deploy/apache2/vhost.conf /etc/apache2/sites-available/000-default.conf
-COPY .deploy/apache2/apache.conf /etc/apache2/conf-available/z-app.conf
-COPY .deploy/index.php /var/www/html/public/index.php
-
-RUN a2enconf z-app
-
-RUN chown -R www-data:www-data /var/www/html
-
-RUN  useradd -u 1000 -m debian && echo "debian:debian" | chpasswd && adduser debian sudo
+RUN /.deploy/create-container.sh
 
 EXPOSE 80
 
 USER debian
 
 WORKDIR /var/www/html
+
+CMD ["/var/bin/startup.sh"]
